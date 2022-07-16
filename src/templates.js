a8716070e9d9d372bc674457f61864798f00c94c
Luna R <snowluna@protonmail.com>
2022-07-16 11:14:02 +0000

    Create templates for profile and cards

---
+++ src/templates.js
@@ -0,0 +1,13 @@
+const profile = require("./template_profile.js");
+const card = require("./template_card.js");
+
+console.log(card);
+class teamProfile {
+   constructor(teamArray) {
+   }
+
+   createCards() {
+   }
+}
+
+module.exports = { teamProfile }
